4098cdc3a5d7f1946a746ce8edddddbbb68bcdb0
Maelle05 <maelle.rabouan@gmail.com>
2021-11-23 17:11:52 +0100

    j2

--- src/base.js
+++ src/base.js
@@ -1,8 +1,13 @@
 import { webgl } from './three'
+import data from './data/data.json';
 
 
 const three = new webgl()
+const colorV = ["#D3AA6B", "#25A0A0", "#F2C94C", "#6B99D3", "#D36B6B", "#BED36B"]
+
+//INIT
 three.init()
+setData(0)
 
 const numVehicules = three.getSteps()
 const navDiv = document.getElementById('Nav')
@@ -11,12 +16,47 @@ let allNavItem = []
 for (let i = 0; i < numVehicules; i++) {
   const newPoint = document.createElement("div")
   newPoint.className = 'navItem'
+  if (i === 0 ) {
+    newPoint.className = 'active navItem'
+  }
+  const date = document.createElement("p")
+  date.innerHTML = data.vehicules[i].epoque.date
+  newPoint.appendChild(date)
   navDiv.appendChild(newPoint)
   allNavItem.push(newPoint)
   newPoint.addEventListener(
     'click',
     () => {
       three.mouveTo(i)
+      setData(i)
+      setColor(i)
     }
   )
+}
+  
+const left = document.getElementById('rotLeft')
+const right = document.getElementById('rotRight')
+
+left.addEventListener('click', ()=>{
+  // console.log('left');
+  three.rotateLeft(three.getCurrentStep())
+})
+
+right.addEventListener('click', ()=>{
+  // console.log('right');
+  three.rotateRight(three.getCurrentStep())
+})
+  
+function setData(i) {
+  const title = document.querySelector('#vehicule > p')
+  title.innerHTML = data.vehicules[i].name
+}
+  
+setColor(0)
+function setColor(step) {
+  three.setColor(colorV[step])
+  for (let i = 0; i < allNavItem.length; i++) {
+    allNavItem[i].style.backgroundColor = `${colorV[step]}`
+    
+  }
 }
--- src/three.js
+++ src/three.js
@@ -3,13 +3,15 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import gsap from 'gsap';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 export class webgl {
   constructor(){
     // Debug
-    this.gui = new dat.GUI()
-    this.guiCam = this.gui.addFolder('Camera position')
-    this.guiVehicules = this.gui.addFolder('Vehicules')
+    // this.gui = new dat.GUI()
+    // this.guiCam = this.gui.addFolder('Camera position')
+    // this.guiVehicules = this.gui.addFolder('Vehicules')
+    this.gltfLoader = new GLTFLoader()
 
     // Canvas
     this.canvas = document.querySelector('canvas.webgl')
@@ -26,11 +28,19 @@ export class webgl {
       gap: 0,
       rotSpeed: .2
     }
+    
+    this.bgCircle = new THREE.Mesh(new THREE.CircleGeometry( 5, 50 ), new THREE.MeshBasicMaterial({color: '#D3AA6B'}))
     this.vehiculesGroup = new THREE.Object3D()
-    this.cart = new THREE.Mesh( new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'grey'}))
+    this.cartG = new THREE.Object3D()
+    this.boatG = new THREE.Object3D()
+    this.oldCarG = new THREE.Object3D()
+    this.trainG = new THREE.Object3D()
+    this.planeG = new THREE.Object3D()
+    this.raceCarG = new THREE.Object3D()
+    this.cart = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'grey'}))
     this.boat = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'grey'}))
     this.oldCar = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'grey'}))
-    this.train = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'grey'}))
+    this.train = 'train'
     this.plane = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'grey'}))
     this.raceCar = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'grey'}))
 
@@ -49,30 +59,74 @@ export class webgl {
     this.curentStep = 0
   }
   init(){
+    this.gltfLoader.load(
+        '/models/Duck/Duck.gltf',
+        (gltf) =>
+        {
+            console.log('success')
+            console.log(gltf.scene.children[0]);
+            this.scene.add(gltf.scene.children[0])
+            // this.train = gltf.scenes[0].children[0].children[1]
+            // console.log(this.train.);
+            // this.trainG.add(this.train)
+        }
+    )
     this.parametre.gap = this.parametre.initPos + 11
+    this.scene.background = new THREE.Color( 0x121212 );
+    // this.scene.background = new THREE.Color( 0xFFFFFF );
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+    this.scene.add(ambientLight)
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+    directionalLight.castShadow = true
+    directionalLight.shadow.mapSize.set(1024, 1024)
+    directionalLight.shadow.camera.far = 15
+    directionalLight.shadow.camera.left = - 7
+    directionalLight.shadow.camera.top = 7
+    directionalLight.shadow.camera.right = 7
+    directionalLight.shadow.camera.bottom = - 7
+    directionalLight.position.set(- 5, 5, 0)
+    this.scene.add(directionalLight)
 
     /**
      * Objects
      */
-    this.cart.position.x = this.parametre.initPos
-    this.boat.position.x = 1 * this.parametre.gap + this.parametre.initPos
-    this.oldCar.position.x = 2 * this.parametre.gap + this.parametre.initPos
-    this.train.position.x = 3 * this.parametre.gap + this.parametre.initPos
-    this.plane.position.x = 4 * this.parametre.gap + this.parametre.initPos
-    this.raceCar.position.x = 5 * this.parametre.gap + this.parametre.initPos
+    this.cartG.position.x = this.parametre.initPos
+    this.boatG.position.x = 1 * this.parametre.gap + this.parametre.initPos
+    this.oldCarG.position.x = 2 * this.parametre.gap + this.parametre.initPos
+    this.trainG.position.x = 3 * this.parametre.gap + this.parametre.initPos
+    this.planeG.position.x = 4 * this.parametre.gap + this.parametre.initPos
+    this.raceCarG.position.x = 5 * this.parametre.gap + this.parametre.initPos
+
+    this.bgCircle.position.x = -8.5
+    this.bgCircle.position.z = -10
+    this.bgCircle.position.y = -2.3
+    this.bgCircle.rotation.x = 6
 
 
     this.vehiculesGroup.position.y = 0.2
-    this.vehiculesGroup.add(this.cart, this.boat, this.oldCar, this.train, this.plane, this.raceCar)
-    this.scene.add(this.vehiculesGroup)
+    this.cartG.add(this.cart)
+    this.boatG.add(this.boat)
+    this.oldCarG.add(this.oldCar)
+    // this.trainG.add(this.train)
+    this.planeG.add(this.plane)
+    this.raceCarG.add(this.raceCar)
+    
+    // this.vehiculesGroup.add(this.cartG, this.boatG, this.oldCarG, this.trainG, this.planeG, this.raceCarG)
+    this.vehiculesGroup.add(this.cartG, this.boatG, this.oldCarG, this.planeG, this.raceCarG)
 
+    this.scene.add(this.vehiculesGroup, this.bgCircle)
     /**
      * Debug
      */
-    this.guiVehicules.add(this.vehiculesGroup.position, 'x', -10, 6, .1)
-    this.guiVehicules.add(this.vehiculesGroup.position, 'y', -6, 6, .1)
-    this.guiVehicules.add(this.vehiculesGroup.position, 'z', -6, 6, .1)
-    this.guiVehicules.add(this.parametre, 'rotSpeed', 0, 1, .1)
+    // this.guiVehicules.add(this.vehiculesGroup.position, 'x', -10, 6, .1)
+    // this.guiVehicules.add(this.vehiculesGroup.position, 'y', -6, 6, .1)
+    // this.guiVehicules.add(this.vehiculesGroup.position, 'z', -6, 6, .1)
+    // this.guiVehicules.add(this.parametre, 'rotSpeed', 0, 1, .1)
+    // this.guiVehicules.add(this.bgCircle.position, 'x', -20, 6, .1)
+    // this.guiVehicules.add(this.bgCircle.position, 'z', -30, 1, .1)
+    // this.guiVehicules.add(this.bgCircle.position, 'y', -10, 10, .1)
 
     window.addEventListener('resize', () =>
     {
@@ -100,9 +154,9 @@ export class webgl {
     /**
      * Debug
      */
-    this.guiCam.add(this.camera.position, 'x', 0, 10, 1)
-    this.guiCam.add(this.camera.position, 'y', 0, 10, 1)
-    this.guiCam.add(this.camera.position, 'z', 0, 10, 1)
+    // this.guiCam.add(this.camera.position, 'x', 0, 10, 1)
+    // this.guiCam.add(this.camera.position, 'y', 0, 10, 1)
+    // this.guiCam.add(this.camera.position, 'z', 0, 10, 1)
 
     /**
      * Controls
@@ -129,8 +183,8 @@ export class webgl {
     {
       this.elapsedTime = this.clock.getElapsedTime()
       // Update cube
-      this.vehiculesGroup.children.forEach(vehicule => {
-        vehicule.rotation.y = this.elapsedTime * this.parametre.rotSpeed
+      this.vehiculesGroup.children.forEach(vehiculeG => {
+        vehiculeG.rotation.y = this.elapsedTime * this.parametre.rotSpeed
       });
 
       // Update controls
@@ -150,12 +204,28 @@ export class webgl {
     return this.steps
   }
 
+  getCurrentStep(){
+    return this.curentStep
+  }
+
   mouveTo(numStep){
     if (this.curentStep != numStep) {
       const goTo = -numStep * this.parametre.gap
       gsap.to(this.vehiculesGroup.position, 1, {x: goTo});
       this.curentStep = numStep
     }
+  }
+
+  setColor(color){
+    this.bgCircle.material.color = new THREE.Color(color)
+  }
+
+  rotateLeft(i){
+    gsap.to(this.vehiculesGroup.children[i].children[0].rotation, 1, {y: this.vehiculesGroup.children[i].children[0].rotation.y - 2});
+  }
+
+  rotateRight(i){
+    gsap.to(this.vehiculesGroup.children[i].children[0].rotation, 1, {y: this.vehiculesGroup.children[i].children[0].rotation.y + 2});
   }
 
 }
